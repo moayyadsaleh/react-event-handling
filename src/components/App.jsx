e256aaab0fc7e8cd28f7b63e979ede58a3bdb9d9
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-08-30 19:34:29 -0600

    Use use state to changhe button color on hover using react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
 import React, { useState } from "react";
 import "../styles.css";
+
 function App() {
   const [headingText, setHeadingText] = useState("Hello");
+  const [isMousedOver, setMouseOver] = useState(false);
+
   function handleClick() {
     setHeadingText("Submitted");
   }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false); // Change state to false when the mouse is out
+  }
+
   return (
     <div className="container">
       <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button onClick={handleClick}>Submit</button>
+      <button
+        style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
     </div>
   );
 }
+
 export default App;
